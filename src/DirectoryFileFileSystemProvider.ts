217e87236da2884512349997759c874bebc48077
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2023-07-27 06:01:07 +1200

    Actually update directoryfile on changes for *all* containing files

--- src/DirectoryFileFileSystemProvider.ts
+++ src/DirectoryFileFileSystemProvider.ts
@@ -1,25 +1,28 @@
-import type { ExtensionContext, FileChangeEvent, FileSystemProvider, FileSystemWatcher, Uri } from "vscode";
-import { Disposable, EventEmitter, FileChangeType, FileType, RelativePattern, workspace } from "vscode";
+import type { ExtensionContext, FileChangeEvent, FileSystemProvider, FileSystemWatcher, RelativePattern, Uri } from "vscode";
+import { Disposable, EventEmitter, FileChangeType, FileType, TabInputText, window, workspace } from "vscode";
 import DirectoryFileScheme from "./DirectoryFileScheme";
 import path = require("path");
 
 export default class DirectoryFileFileSystemProvider implements FileSystemProvider {
 
 	public static register (context: ExtensionContext) {
-		const provider = new DirectoryFileFileSystemProvider();
+		const provider = new DirectoryFileFileSystemProvider(context);
 		const providerRegistration = workspace.registerFileSystemProvider(DirectoryFileScheme.SCHEME, provider, { isCaseSensitive: true });
 		context.subscriptions.push(providerRegistration);
 	}
 
-	private emitter = new EventEmitter<FileChangeEvent[]>();
-	public onDidChangeFile = this.emitter.event;
-
-	private watchers = new Map<string, FileSystemWatcher>();
+	private constructor (context: ExtensionContext) {
+		const watchRelevantFiles = this.watchRelevantFiles.bind(this);
+		context.subscriptions.push(window.tabGroups.onDidChangeTabGroups(watchRelevantFiles));
+		context.subscriptions.push(window.onDidChangeVisibleTextEditors(watchRelevantFiles));
+	}
 
 	public async stat (uri: Uri) {
 		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
 		const result = await workspace.fs.stat(realUri);
+		const tracked = this.trackedDirectoryFiles.get(uri.path);
 		result.type = FileType.File;
+		result.mtime = tracked?.mtime || result.mtime;
 		return result;
 	}
 
@@ -32,7 +35,7 @@ export default class DirectoryFileFileSystemProvider implements FileSystemProvid
 		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
 		const workspaceFolderPath = workspace.getWorkspaceFolder(realUri)?.uri.path ?? workspace.rootPath ?? "/";
 		return mergeUint8Arrays([
-			new TextEncoder().encode(`Viewing directory as file: ${relative(workspaceFolderPath, uri.path)}\n`),
+			new TextEncoder().encode(`Viewing directory as file: ${relative(workspaceFolderPath, realUri.path)}\n`),
 			await this.readFileOrDirectoryAsCombinedFile(realUri, realUri),
 		]);
 	}
@@ -53,8 +56,11 @@ export default class DirectoryFileFileSystemProvider implements FileSystemProvid
 
 		return file ?? Promise.resolve(workspace.fs.readDirectory(uri))
 			.catch(() => [])
-			.then(files => files.map(([file]) => uri.with({ path: path.join(uri.fsPath, file) })))
-			.then(uris => Promise.all(uris.map(async uri => ({ uri, data: await this.readFileOrDirectoryAsCombinedFile(uri, root) }))))
+			.then(files => files
+				.sort(([a], [b]) => a.localeCompare(b))
+				.map(([file]) => uri.with({ path: path.join(uri.fsPath, file) })))
+			.then(uris => Promise.all(uris
+				.map(async uri => ({ uri, data: await this.readFileOrDirectoryAsCombinedFile(uri, root) }))))
 			.then(files => mergeUint8Arrays(files.map(file => file.data)));
 	}
 
@@ -70,38 +76,7 @@ export default class DirectoryFileFileSystemProvider implements FileSystemProvid
 	}
 
 	public watch (uri: Uri, options: { readonly recursive: boolean; readonly excludes: readonly string[]; }): Disposable {
-		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
-
-		// Create a glob pattern for the provided URI, based on the options
-		const globPattern = new RelativePattern(realUri.fsPath, "**/*");
-
-		// Create the file system watcher
-		const fileWatcher = workspace.createFileSystemWatcher(globPattern, options.recursive, false, false);
-
-		if (options.excludes.length)
-			throw new Error("Cannot use `excludes` as vscode does not expose glob support");
-
-		// Add event listeners for the file system watcher
-		fileWatcher.onDidCreate(this.createFileChangeHandler(FileChangeType.Created));
-		fileWatcher.onDidChange(this.createFileChangeHandler(FileChangeType.Changed));
-		fileWatcher.onDidDelete(this.createFileChangeHandler(FileChangeType.Deleted));
-
-		// Store the watcher in the map for future disposal if needed
-		this.watchers.set(uri.fsPath, fileWatcher);
-
-		// Return a disposable object to unregister the watcher when necessary
-		return Disposable.from(fileWatcher, {
-			dispose: () => {
-				fileWatcher.dispose();
-				this.watchers.delete(uri.fsPath);
-			},
-		});
-	}
-
-	private createFileChangeHandler (type: FileChangeType, excludes?: RelativePattern[]) {
-		return (uri: Uri) => {
-			this.emitter.fire([{ uri, type }]);
-		};
+		return this.addWatchReason(uri);
 	}
 
 	public delete (uri: Uri, options: { readonly recursive: boolean; }): void | Thenable<void> {
@@ -112,6 +87,89 @@ export default class DirectoryFileFileSystemProvider implements FileSystemProvid
 	public createDirectory (uri: Uri): void | Thenable<void> {
 		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
 		return workspace.fs.createDirectory(realUri);
+	}
+
+	private emitter = new EventEmitter<FileChangeEvent[]>();
+	public onDidChangeFile = this.emitter.event;
+
+	private trackedDirectoryFiles = new Map<string, {
+		mtime: number,
+		watcher: FileSystemWatcher,
+		reasons: Set<number>,
+	}>();
+
+	private watchRelevantFiles () {
+		const openTabs = window.tabGroups.all
+			.flatMap(group => group.tabs)
+			.filter(tab => tab.input instanceof TabInputText && tab.input.uri.path.endsWith(DirectoryFileScheme.EXT));
+
+		// todo set tab labels when vscode finally supports that
+
+		const openFiles = openTabs.map(tab => (tab.input as TabInputText).uri);
+
+		for (const file of openFiles)
+			this.addWatchReason(file, -1);
+
+		const openFileKeys = new Set(openFiles.map(file => file.path));
+		// console.log("visible files:", ...openFileKeys);
+		for (const path of [...this.trackedDirectoryFiles.keys()])
+			if (!openFileKeys.has(path))
+				this.removeWatchReason(path, -1);
+	}
+
+	private watchId = 0;
+
+	private addWatchReason (uri: Uri, id = this.watchId++) {
+		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
+
+		let managedWatcher = this.trackedDirectoryFiles.get(uri.path);
+		if (!managedWatcher) {
+
+			const relativePath = path.relative(workspace.getWorkspaceFolder(realUri)?.uri.path ?? "/", realUri.path);
+
+			const glob = path.join(relativePath, "**/*").replace(REGEX_SLASH, "/");
+
+			const fileWatcher = workspace.createFileSystemWatcher("**/*");
+			// console.log("create file watcher", glob);
+
+			fileWatcher.onDidCreate(this.createFileChangeHandler(uri, FileChangeType.Created));
+			fileWatcher.onDidChange(this.createFileChangeHandler(uri, FileChangeType.Changed));
+			fileWatcher.onDidDelete(this.createFileChangeHandler(uri, FileChangeType.Deleted));
+
+			this.trackedDirectoryFiles.set(uri.path, managedWatcher = {
+				mtime: 0,
+				watcher: fileWatcher,
+				reasons: new Set(),
+			});
+		}
+
+		managedWatcher.reasons.add(id);
+
+		return new Disposable(() => this.removeWatchReason(uri.path, id));
+	}
+
+	private removeWatchReason (path: string, id: number) {
+		const managedWatcher = this.trackedDirectoryFiles.get(path);
+		managedWatcher!.reasons.delete(id);
+		if (!managedWatcher!.reasons.size) {
+			// console.log("dispose file watcher", path);
+			managedWatcher!.watcher.dispose();
+			this.trackedDirectoryFiles.delete(path);
+		}
+	}
+
+	private createFileChangeHandler (uri: Uri, type: FileChangeType, excludes?: RelativePattern[]) {
+		const realUri = DirectoryFileScheme.decodeToDirectoryUri(uri);
+		return (changedFileUri: Uri) => {
+			if (changedFileUri.path.startsWith(realUri.path)) {
+				// console.log("file changed", changedFileUri.path, "updating", uri.path);
+				const tracked = this.trackedDirectoryFiles.get(uri.path);
+				if (tracked)
+					tracked.mtime = Date.now();
+
+				this.emitter.fire([{ uri, type }]);
+			}
+		};
 	}
 }
 
--- src/DirectoryFileScheme.ts
+++ src/DirectoryFileScheme.ts
@@ -1,17 +1,19 @@
+import path = require("path");
 import { Uri } from "vscode";
 
 namespace DirectoryFileScheme {
 
 	export const SCHEME = "directoryfile";
+	export const EXT = ". (Directory)"
 
 	let seq = 0;
 
 	export function encodeToDirectoryFileUri (uri: Uri): Uri {
-		return Uri.parse(`${SCHEME}:${encodeURIComponent(uri.path)}#${seq++}`);
+		return Uri.parse(`${SCHEME}:${encodeURIComponent(uri.path)}${EXT}#${seq++}`);
 	}
 
 	export function decodeToDirectoryUri (uri: Uri): Uri {
-		return Uri.parse(`file:${decodeURIComponent(uri.path)}`);
+		return Uri.parse(`file:${decodeURIComponent(uri.path).slice(0, -EXT.length)}`);
 	}
 }
 
